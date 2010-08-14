353d6585f25bbf7da10b1cbb1daad6b32bb91e60
Henrikke Pereira <ikkibr@gmail.com>
2010-08-14 18:53:17 -0300

    fixed urls

--- dashline/apps/timeline/urls.py
+++ dashline/apps/timeline/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 urlpatterns = patterns('',
     url(r'^show/(?P<slug>\S+)$','timeline.views.show_timeline', name='timeline_show'),
     url(r'^create/$','timeline.views.create_timeline', name='timeline_create'),
-    url(r'^browser/$','timeline.views.browse_timelines', name='timeline_browse'),
-    url(r'^add_entries/(?P<slug>\S+)$','timeline.views.browse_timelines', name='timeline_add_entries'),
+    url(r'^browse/$','timeline.views.browse_timelines', name='timeline_browse'),
+    url(r'^add_entries/(?P<slug>\S+)$','timeline.views.add_entries', name='timeline_add_entries'),
 )
