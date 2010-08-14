e5889395b17815b2235dd832f9a9481d261a4303
Felipe 'chronos' Prenholato <philipe.rp@gmail.com>
2010-08-14 18:00:09 -0300

    Adding criauser.py script: scripts/criauser.py username email@example.com password ......... just for tests.

---
+++ dashline/scripts/criauser.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os,sys
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'dashline.settings'
+
+import dashline
+sys.path.insert(0,os.path.dirname(dashline.__file__))
+
+from django.contrib.auth.models import User
+
+try:
+    u = User.objects.get(username=sys.argv[1])
+    u.delete()
+except User.DoesNotExist:
+    u = User(username=sys.argv[1],email=sys.argv[2])
+    u.save()
+    u.set_password(sys.argv[3])
+    u.save()
