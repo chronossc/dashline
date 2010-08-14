4920057a7d4a80828a62f55da5c149fbe8097c59
Diógenes <diofeher@gmail.com>
2010-08-14 16:57:29 -0300

    added gravatar support

--- dashline/apps/users/models.py
+++ dashline/apps/users/models.py
@@ -1,7 +1,21 @@
+import urllib
+import hashlib
+
 from django.db import models
 from django.contrib.auth.models import User
 from apps.timeline.models import TimeLine
 
 class UserProfile(models.Model):
-    timelines = models.ForeignKey(TimeLine)
     user = models.ForeignKey(User, unique=True)
+
+    @property
+    def avatar(self):
+        size = 48
+        email = self.user.email
+        default = "http://api.ning.com/files/L-2Mry07IJ3j5NpZFDRiGd2muhP-KJTK*tZ47YoxU2Z2vxpOlFQ4Coi032VORAdYcgx1GXmpfhKedtS9I0gpSoTvg61lWXpX/your_8bit_avatar2.jpg?width=48&height=48&crop=1%3A1"
+        url = "http://www.gravatar.com/avatar.php?%s" % urllib.urlencode({
+                                                                        'gravatar_id': hashlib.md5(email).hexdigest(), 
+                                                                        'default': default, 
+                                                                        'size': str(size) })
+        gravatar = {'url': url, 'size': size}
+        return '<img width="%s" src="%s" alt="%s"></img>' % (size, gravatar['url'], gravatar['size'])
