bfb45516abaa3cb02d780c82e2283a37ea4cc36c
Diógenes <diofeher@gmail.com>
2010-08-14 02:26:09 -0300

    added basic structure

---
+++ dashline/apps/users/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__
+
+    description
+
+    @copyright: 2010 DiogenesAugusto <diofeher@gmail.com>
+    @license: GNU GPL.
+"""
---
+++ dashline/apps/users/models.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    models
+
+    description
+
+    @copyright: 2010 DiogenesAugusto <diofeher@gmail.com>
+    @license: GNU GPL.
+"""
