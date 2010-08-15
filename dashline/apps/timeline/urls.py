b525d47fa8252ef2c6993c522b8125870e70e2f8
Felipe 'chronos' Prenholato <philipe.rp@gmail.com>
2010-08-15 02:53:00 -0300

    timeline home page, just basic.

--- dashline/apps/timeline/urls.py
+++ dashline/apps/timeline/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 urlpatterns = patterns('',
+    url(r'^$','timeline.views.home_timeline', name='timeline_home'),
     url(r'^show/(?P<slug>\S+)?$','timeline.views.show_timeline', name='timeline_show'),
     url(r'^create/$','timeline.views.create_timeline', name='timeline_create'),
     url(r'^browse/$','timeline.views.browse_timelines', name='timeline_browse'),
