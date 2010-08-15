46030d9022ca616ee468f6030b0052db032b2877
dashline <dashline@nulllenny.dreamhost.com>
2010-08-15 11:31:35 -0700

    Fix update_index script to run everywhere

--- dashline/scripts/update_search_indexes.sh
+++ dashline/scripts/update_search_indexes.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-python ../dashline/manage.py update_index
+
+[[ -f /home/dashline/.bash_profile ]] && source /home/dashline/.bash_profile
+
+export DJANGO_SETTINGS_MODULE='dashline.settings'
+
+ABSPATH=$(dirname $0)
+
+python $ABSPATH/../manage.py update_index
