9be3ae151725f3e9932232155a90b89a72637a3f
Diógenes <diofeher@gmail.com>
2010-08-14 22:22:48 -0300

    fixed fields of haystack search

--- dashline/search_sites.py
+++ dashline/search_sites.py
@@ -10,18 +10,12 @@ from users.models import UserProfile
 
 class TimeLineIndex(SearchIndex):
     text = CharField(document=True, use_template=True)
-    author = CharField(model_attr='user')
-    title = CharField(model_attr='title')
-    description = CharField(model_attr='description')
+    owner = CharField(model_attr='owner')
 
 
 class UserProfileIndex(SearchIndex):
     text = CharField(document=True, use_template=True)
     user = CharField(model_attr='user')
-    username = CharField(model_attr='user.name')
-    twitter = CharField(model_attr='twitter')
-    description = CharField(model_attr='description')
-    
 
 
 site.register(TimeLine, TimeLineIndex)
