fdbde61d5e8aa1c9acd0dba229ca0e0065978743
Diógenes <diofeher@gmail.com>
2010-08-14 23:48:14 -0300

    fixed friend add

--- dashline/apps/users/views.py
+++ dashline/apps/users/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import render_to_response, get_object_or_404
+from django.core.urlresolvers import reverse
 from models import Follow
 
 @login_required
@@ -11,7 +12,7 @@ def users_follow(request, *args, **kwargs):
         friend = get_object_or_404(User, username=username)
         follow = Follow(from_user=request.user, to_user=friend)
         follow.save()
-        messages.add_message(request, messages.INFO, '.')
-        return HttpResponseRedirect('/friends/%s/' % request.user.username)
+        messages.add_message(request, messages.INFO, 'You are now following %s' % friend.user.username)
+        return reverse('profiles_profile_detail', args=[friend.user.username])
     except:
         raise Http404()
